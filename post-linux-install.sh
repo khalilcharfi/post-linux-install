fbb0312e1e43c603efe40674bd6a4657c85b28f3
Mateus Junges <mateus@junges.dev>
2020-09-22 12:14:13 -0300

    Add GH CLI

--- post-linux-install.sh
+++ post-linux-install.sh
@@ -124,3 +124,9 @@ sudo service apache2 restart
 
 # Install supervisor
 sudo apt-get install supervisor
+
+# Install GH CLI
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+sudo apt-add-repository https://cli.github.com/packages
+sudo apt update
+sudo apt install gh
